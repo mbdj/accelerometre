1fcc4e5f04ce067148d7936f23221909ae69715c
mehdi <mbdj@free.fr>
2022-06-28 23:20:34 +0200

    ssd1306 fonctionne

--- src/my_i2c.adb
+++ src/my_i2c.adb
@@ -2,22 +2,16 @@ with STM32.Setup;
 
 package body My_I2C is
 
+	procedure Initialize is
+	begin
 
-   function  Initialize return Boolean is
-   begin
+		STM32.Setup.Setup_I2C_Master  (Port => I2C_1,
+											SDA => Screen_I2C_SDA,
+											SCL => Screen_I2C_SCL,
+											SDA_AF => GPIO_AF_I2C1_4,
+											SCL_AF => GPIO_AF_I2C1_4,
+											Clock_Speed => 100_000); -- 100 MHz
 
-      STM32.Setup.Setup_I2C_Master (Port        => I2C_1,
-                                    SDA         => Screen_I2C_SDA,
-                                    SCL         => Screen_I2C_SCL,
-                                    SDA_AF      => GPIO_AF_I2C1_4,
-                                    SCL_AF      => GPIO_AF_I2C1_4,
-                                    Clock_Speed => 100_000);
-
-      return True;
-
-
-   end Initialize;
-
-
+	end Initialize;
 
 end My_I2C;
--- src/my_i2c.ads
+++ src/my_i2c.ads
@@ -1,11 +1,10 @@
-with STM32.GPIO;    use STM32.GPIO;
-with STM32.Device;  use STM32.Device;
-
+with STM32.GPIO;   use STM32.GPIO;
+with STM32.Device; use STM32.Device;
 
 package My_I2C is
-   Screen_I2C_SDA : GPIO_Point renames PB7;
-   Screen_I2C_SCL : GPIO_Point renames PB6;
+   Screen_I2C_SDA : GPIO_Point renames PB9;
+   Screen_I2C_SCL : GPIO_Point renames PB8;
 
-   function Initialize return Boolean;
+   procedure Initialize;
 
 end My_I2C;
